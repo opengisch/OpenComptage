71a5b8973bd1c5051e4da420015c4e7de4fff343
Mario Baranzini <mario@opengis.ch>
2018-09-04 09:31:55 +0200

    Add tests structure with docker

---
+++ .docker/run-docker-tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#***************************************************************************
+#                             -------------------
+#       begin                : 2017-08-24
+#       git sha              : :%H$
+#       copyright            : (C) 2017 by OPENGIS.ch
+#       email                : info@opengis.ch
+#***************************************************************************
+#
+#***************************************************************************
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU General Public License as published by  *
+#*   the Free Software Foundation; either version 2 of the License, or     *
+#*   (at your option) any later version.                                   *
+#*                                                                         *
+#***************************************************************************
+
+set -e
+# rationale: Wait for postgres container to become available
+echo "Wait a moment while loading the database."
+while ! PGPASSWORD='docker' psql -h postgres -U docker -p 5432 -l &> /dev/null
+do
+  printf "."
+  sleep 2
+done
+echo ""
+
+pushd /usr/src
+xvfb-run nose2-3
+popd
---
+++ comptages/test/README.md
@@ -0,0 +1,23 @@
+# Running the tests
+
+To run the tests inside the same environment as they are executed on Travis,
+you need a [Docker](https://www.docker.com/) installation. This will also launch an extra container
+with a database, so your own postgres installation is not affected at all.
+
+To run the tests, go to the main directory of the project and do
+
+```sh
+export TRAVIS_BUILD_DIR=$PWD # only for local execution
+docker-compose -f .docker/docker-compose.travis.yml run qgis /usr/src/.docker/run-docker-tests.sh
+```
+
+In one line, removing all containers.
+```sh
+TRAVIS_BUILD_DIR=$PWD docker-compose -f .docker/docker-compose.travis.yml run qgis /usr/src/.docker/run-docker-tests.sh; TRAVIS_BUILD_DIR=$PWD docker-compose -f .docker/docker-compose.travis.yml rm -s -f
+```
+
+To rebuild the containers.
+```sh
+export TRAVIS_BUILD_DIR=$PWD # only for local execution
+docker-compose -f .docker/docker-compose.travis.yml build
+```
