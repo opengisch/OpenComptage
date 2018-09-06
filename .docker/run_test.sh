be26ba85490f56137826f5cad3e55ef90edd8f91
Mario Baranzini <mario@opengis.ch>
2018-09-06 14:10:28 +0200

    Cleanup

--- .docker/run_test.sh
+++ .docker/run_test.sh
@@ -2,7 +2,6 @@
 Xvfb :1 -screen 0 1024x768x16 &
 DISPLAY=:1.0
 export DISPLAY
-find / -type d -name 'QGIS' >> /tests/find.txt 
 
 echo "Launching QGIS with startup script"
 qgis --version-migration --nologo --code /tests/test.py --profiles-path /tests/ &
