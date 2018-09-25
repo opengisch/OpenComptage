48d2b5ae29374ac3dba72989240c4765c0156fe1
Mario Baranzini <mario@opengis.ch>
2018-09-25 16:28:08 +0200

    Import section data from existing base_tjm_ok

---
+++ db/base_tjm_ok_data_transfer.sql
@@ -0,0 +1,12 @@
+
+-- Transfer data from the table base_tjm_ok copy-pasted in QGIS to the real tables  
+
+SET search_path = base_tjm_ok, comptages;
+
+insert into section(id, name, owner, road, way, start_pr, end_pr, start_dist, end_dist, place_name, geometry) 
+	select fsection, nom_rue,
+	f_prop, f_axe, f_sens, f_pr_d, f_pr_f, 
+	to_number(f_dist_d, '999999999999999999.999'), 
+	to_number(f_dist_f, '999999999999999999.999'), 
+	lieu_rue,
+	geometry from base_tjm_ok;
--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-psql "service=comptages_dev" --single-transaction -f ../db/comptages.sql
+psql "service=comptages_dev" --single-transaction --echo-errors -f ../db/comptages.sql
+psql "service=comptages_dev" --single-transaction --echo-errors -f ../db/base_tjm_ok.sql 
+psql "service=comptages_dev" --single-transaction --echo-errors -f ../db/base_tjm_ok_data.sql
+psql "service=comptages_dev" --single-transaction --echo-errors -f ../db/base_tjm_ok_data_transfer.sql 
