79cb1dde1882bc2e624a5a411cde99b5321cd284
Mario Baranzini <marioba@users.noreply.github.com>
2019-01-22 08:17:57 +0100

    Restore docker tests (#41)
    Run tests on official qgis docker

--- comptages/test/functional.py
+++ comptages/test/functional.py
@@ -2,17 +2,21 @@
 
 from qgis.PyQt.QtWidgets import QToolBar
 
-import unittest
-import os
-from qgis.utils import iface, active_plugins, startPlugin, loadPlugin
+import sys
+from qgis.testing import unittest
+from qgis.core import QgsProject
+from qgis.utils import iface, active_plugins
+
+
+def run_all():
+    """Default function that is called by the runner if nothing else is
+    specified"""
+    suite = unittest.TestSuite()
+    suite.addTests(unittest.makeSuite(TestFunc, 'test'))
+    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(suite)
 
 
 class TestFunc(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        loadPlugin('comptages')
-        startPlugin('comptages')
 
     def test_plugin_is_active(self):
         self.assertIn('comptages', active_plugins)
@@ -22,14 +26,17 @@ class TestFunc(unittest.TestCase):
         # Julie sees the plugin toolbar with the "connect_db" and the
         # "settings" buttons enabled and the other disabled
         self.assertTrue(TestFunc.get_comptages_toolbar())
-        self.assertTrue(TestFunc.is_action_enabled("Connect DB"))
-        self.assertTrue(TestFunc.is_action_enabled("Settings"))
-        #self.assertFalse(TestFunc.is_action_enabled("Create new measure"))
-        #self.assertFalse(TestFunc.is_action_enabled("Edit measure"))
-        #self.assertFalse(TestFunc.is_action_enabled("Filter"))
+        self.assertTrue(TestFunc.is_action_enabled("Connection DB"))
+        self.assertTrue(TestFunc.is_action_enabled("Réglages"))
+        self.assertFalse(
+            TestFunc.is_action_enabled("Créer un nouveau comptage"))
+        self.assertFalse(TestFunc.is_action_enabled("Modifier comptage"))
+        self.assertFalse(TestFunc.is_action_enabled("Importation"))
+        self.assertFalse(TestFunc.is_action_enabled("Validation"))
+        self.assertFalse(TestFunc.is_action_enabled("Filtrer"))
 
         # Julie presses on the start button of the plugin
-        TestFunc.trigger_action("Connect DB")
+        TestFunc.trigger_action("Connection DB")
 
         # She sees that the layers are loaded
         self.assertTrue(
@@ -38,11 +45,18 @@ class TestFunc(unittest.TestCase):
             TestFunc.layer_is_loaded("troncon"))
 
         # And all the buttons are enabled
-        self.assertTrue(TestFunc.is_action_enabled("Connect DB"))
-        self.assertTrue(TestFunc.is_action_enabled("Settings"))
-        self.assertTrue(TestFunc.is_action_enabled("Create new measure"))
-        self.assertTrue(TestFunc.is_action_enabled("Edit measure"))
-        self.assertTrue(TestFunc.is_action_enabled("Filter"))
+        self.assertTrue(TestFunc.is_action_enabled("Connection DB"))
+        self.assertTrue(TestFunc.is_action_enabled("Réglages"))
+        self.assertTrue(
+            TestFunc.is_action_enabled("Créer un nouveau comptage"))
+        self.assertTrue(TestFunc.is_action_enabled("Modifier comptage"))
+        self.assertTrue(TestFunc.is_action_enabled("Importation"))
+        self.assertTrue(TestFunc.is_action_enabled("Validation"))
+        self.assertTrue(TestFunc.is_action_enabled("Filtrer"))
+
+        # The features of the section layer are around 8000
+        self.assertGreater(TestFunc.get_layer("troncon").featureCount(), 7500)
+        self.assertLess(TestFunc.get_layer("troncon").featureCount(), 9000)
 
     @staticmethod
     def trigger_action(text):
@@ -73,21 +87,8 @@ class TestFunc(unittest.TestCase):
             return True
         return False
 
-
-if __name__ == "__main__" or __name__ == "__console__":
-    print("--- Functional tests ---")
-    iface.openMessageLog()
-    result = unittest.result.TestResult()
-    test_loader = unittest.TestLoader()
-    test_loader.loadTestsFromTestCase(TestFunc).run(result)
-    print(result)
-    exit_code = 0
-    for _ in result.failures:
-        exit_code = 1
-        print(_)
-
-    for _ in result.errors:
-        exit_code = 1
-        print(_)
-
-    os._exit(exit_code)
+    @staticmethod
+    def get_layer(name):
+        for layer in QgsProject.instance().mapLayersByName(name):
+            return layer
+        return None
