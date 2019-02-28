9dfd83041f6f5bd67e4d42ebf5bffd55c01bbf79
Mario Baranzini <mario@opengis.ch>
2019-02-28 10:38:10 +0100

    Manage multi week reports

--- comptages/report/report_creator.py
+++ comptages/report/report_creator.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from openpyxl import load_workbook
 from qgis.PyQt.QtCore import QDate
 from comptages.core.settings import Settings
@@ -14,92 +15,95 @@ class ReportCreator():
         self.layers = layers
 
     def run(self):
-        # TODO: Evaluate if more than a report file are to be created i.e. more
-        # weeks or special cases
+        # TODO: Evaluate special cases
 
         # FIXME: section_id
         self.section_id = '01743230'
-        print(self.count_id)
-        print(self.section_id)
+
         data_loader = DataLoader(
             self.count_id, self.section_id, Layers.IMPORT_STATUS_DEFINITIVE)
 
-        self.count_data = data_loader.load()
-        self._export_report()
+        count_data = data_loader.load()
 
-    def _export_report(self):
+        for i in range(int(len(count_data.day_data)/7)):
+            self._export_report(count_data, i*7, i*7+6)
+
+    def _export_report(self, count_data, start_day, end_day):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         template = os.path.join(current_dir, 'template.xlsx')
-        self.wb = load_workbook(filename=template)
+        wb = load_workbook(filename=template)
 
-        self._set_data_count()
-        self._set_data_day()
-        self._set_data_speed()
-        self._set_data_category()
+        self._set_data_count(wb, count_data, start_day, end_day)
+        self._set_data_day(wb, count_data, start_day, end_day)
+        self._set_data_speed(wb, count_data, start_day, end_day)
+        self._set_data_category(wb, count_data, start_day, end_day)
 
         # Save the file
-        output = os.path.join(current_dir, self.file_path)
-        self.wb.save(filename=output)
+        week_num = datetime.date(
+            count_data.attributes['dates'][start_day][0],
+            count_data.attributes['dates'][start_day][1],
+            count_data.attributes['dates'][start_day][2]).strftime('%V')
+        output = '{}_S{}.xlsx'.format(self.file_path[0:-5], week_num)
+        wb.save(filename=output)
 
-    def _set_data_count(self):
-        ws = self.wb['Data_count']
+    def _set_data_count(self, workbook, count_data, start_day, end_day):
+        ws = workbook['Data_count']
 
         ws['B3'] = ('Poste de comptage : {}  Axe : {}:{}:  '
                     'PR {} + {} m à PR {} + {} m').format(
                         self.section_id,
-                        self.count_data.attributes['owner'],
-                        self.count_data.attributes['road'],
-                        self.count_data.attributes['start_pr'],
-                        self.count_data.attributes['start_dist'],
-                        self.count_data.attributes['end_pr'],
-                        self.count_data.attributes['end_dist'])
+                        count_data.attributes['owner'],
+                        count_data.attributes['road'],
+                        count_data.attributes['start_pr'],
+                        count_data.attributes['start_dist'],
+                        count_data.attributes['end_pr'],
+                        count_data.attributes['end_dist'])
 
         ws['B4'] = 'Periode de comptage du {}/{}/{} au {}/{}/{}'.format(
-            self.count_data.attributes['dates'][0][2],
-            self.count_data.attributes['dates'][0][1],
-            self.count_data.attributes['dates'][0][0],
-            self.count_data.attributes['dates'][6][2],
-            self.count_data.attributes['dates'][6][1],
-            self.count_data.attributes['dates'][6][0],)
+            count_data.attributes['dates'][start_day][2],
+            count_data.attributes['dates'][start_day][1],
+            count_data.attributes['dates'][start_day][0],
+            count_data.attributes['dates'][end_day][2],
+            count_data.attributes['dates'][end_day][1],
+            count_data.attributes['dates'][end_day][0],)
 
         ws['B5'] = 'Comptage {}'.format(
-            self.count_data.attributes['dates'][0][0])
+            count_data.attributes['dates'][start_day][0])
 
         ws['B6'] = 'Type de capteur : {}'.format(
-            self.count_data.attributes['sensor_type'])
+            count_data.attributes['sensor_type'])
         ws['B7'] = 'Modèle : {}'.format(
-            self.count_data.attributes['model'])
+            count_data.attributes['model'])
         ws['B8'] = 'Classification : {}'.format(
-            self.count_data.attributes['class'])
+            count_data.attributes['class'])
 
         ws['B9'] = 'Comptage véhicule par véhicule'
-        if self.count_data.attributes['aggregate']:
+        if count_data.attributes['aggregate']:
             ws['K5'] = 'Comptage par interval'
 
         ws['B10'] = 'Periode speciales : {}'.format(
             self.layers.check_dates(
                 QDate(
-                    self.count_data.attributes['dates'][0][0],
-                    self.count_data.attributes['dates'][0][1],
-                    self.count_data.attributes['dates'][0][2]),
+                    count_data.attributes['dates'][start_day][0],
+                    count_data.attributes['dates'][start_day][1],
+                    count_data.attributes['dates'][start_day][2]),
                 QDate(
-                    self.count_data.attributes['dates'][6][0],
-                    self.count_data.attributes['dates'][0][1],
-                    self.count_data.attributes['dates'][0][2])))
+                    count_data.attributes['dates'][end_day][0],
+                    count_data.attributes['dates'][end_day][1],
+                    count_data.attributes['dates'][end_day][2])))
 
-        ws['B11'] = self.count_data.attributes['place_name']
+        ws['B11'] = count_data.attributes['place_name']
 
         ws['B12'] = 'Remarque : {}'.format(
-            self.count_data.attributes['remarks']
-            if type(self.count_data.attributes['remarks']) == str else '')
+            count_data.attributes['remarks']
+            if type(count_data.attributes['remarks']) == str else '')
 
-        ws['B13'] = self.count_data.attributes['dir1']
-        ws['B14'] = self.count_data.attributes['dir2']
+        ws['B13'] = count_data.attributes['dir1']
+        ws['B14'] = count_data.attributes['dir2']
 
-    def _set_data_day(self):
-        ws = self.wb['Data_day']
-
-        days = self.count_data.day_data[0:7]
+    def _set_data_day(self, workbook, count_data, start_day, end_day):
+        ws = workbook['Data_day']
+        days = count_data.day_data[start_day:end_day+1]
 
         day_cols_tot = ['B', 'C', 'D', 'E', 'F', 'G', 'H']
         section_start_cell = 5
@@ -130,16 +134,17 @@ class ReportCreator():
             ws['{}{}'.format(day_cols_tot[i], dir2_heavy_cell)] = \
                 day_data.heavy_vehicles(1)
 
-    def _set_data_speed(self):
-        ws = self.wb['Data_speed']
+    def _set_data_speed(self, workbook, count_data, start_day, end_day):
+        ws = workbook['Data_speed']
 
         speed_cols = ['B', 'C', 'D', 'E', 'F', 'G', 'H',
                       'I', 'J', 'K', 'L', 'M', 'N']
         dir1_start_cell = 5
         dir2_start_cell = 33
 
-        dir1 = self.count_data.speed_cumulus(0, days=[0, 1, 2, 3, 4, 5, 6])
-        dir2 = self.count_data.speed_cumulus(1, days=[0, 1, 2, 3, 4, 5, 6])
+        days_idx = [x for x in range(start_day, end_day+1)]
+        dir1 = count_data.speed_cumulus(0, days=days_idx)
+        dir2 = count_data.speed_cumulus(1, days=days_idx)
 
         for i, hour in enumerate(dir1):
             for j, speed in enumerate(hour):
@@ -149,8 +154,8 @@ class ReportCreator():
             for j, speed in enumerate(hour):
                 ws['{}{}'.format(speed_cols[j], i+dir2_start_cell)] = speed
 
-    def _set_data_category(self):
-        ws = self.wb['Data_category']
+    def _set_data_category(self, workbook, count_data, start_day, end_day):
+        ws = workbook['Data_category']
 
         cat_cols = ['B', 'C', 'D', 'E', 'F', 'G', 'H',
                     'I', 'J', 'K']
@@ -158,8 +163,9 @@ class ReportCreator():
         dir1_start_cell = 5
         dir2_start_cell = 33
 
-        dir1 = self.count_data.category_cumulus(0, days=[0, 1, 2, 3, 4, 5, 6])
-        dir2 = self.count_data.category_cumulus(1, days=[0, 1, 2, 3, 4, 5, 6])
+        days_idx = [x for x in range(start_day, end_day+1)]
+        dir1 = count_data.category_cumulus(0, days=days_idx)
+        dir2 = count_data.category_cumulus(1, days=days_idx)
 
         for i, hour in enumerate(dir1):
             for j, cat in enumerate(hour):
