573053550e03700a54305aae48f7b29d310e33da
olivierdalang <olivier.dalang@gmail.com>
2021-06-08 17:03:30 +0200

    replace sections import script by cleaned data and import script

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -12,18 +12,13 @@ then
     psql "service=comptages_dev" --echo-errors -c 'DROP SCHEMA IF EXISTS transfer CASCADE;'
 
     # Import the data model into the database
-    psql "service=comptages_dev" --single-transaction --echo-errors -f ../db/generated_model_script.sql
+    python manage.py migrate
 
     # Import domain defined data (e.g. classes and categories)
     psql "service=comptages_dev" --single-transaction --echo-errors -f ../db/domain_data.sql
-    echo "before ogr2ogr"
-    # Import the base_tjm_ok MapInfo dump into the transfer schema
-    psql "service=comptages_dev" --echo-errors -c 'CREATE SCHEMA IF NOT EXISTS transfer;'
-    ogr2ogr -f "PostgreSQL" PG:"service=comptages_dev schemas=transfer" -t_srs EPSG:2056 -overwrite ../db/legacy/base_tjm_ok_20180227/BASE_TJM_OK.TAB
-    echo "ogr2ogr finished"
-    python --version
-    # Import data from base_tjm to the correct tables
-    ./transfer_base_tjm_ok.py
+
+    # Import all sections
+    python manage.py importsections
 
     # Set the log properties
     psql "service=comptages_dev" --echo-errors -f ../db/audit.sql
