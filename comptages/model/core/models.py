e95d2ed38bb612049e6eee6590c113120b81c76c
olivierdalang <olivier.dalang@gmail.com>
2021-06-08 14:15:13 +0200

    Adjust model definition and create migrations

--- comptages/model/core/models.py
+++ comptages/model/core/models.py
@@ -1,11 +1,8 @@
-# This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
+
+from django.contrib.gis.db import models
 
 
 class BaseTjmOk(models.Model):
@@ -42,36 +39,35 @@ class BaseTjmOk(models.Model):
     wkb_geometry = models.GeometryField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'base_tjm_ok'
 
 
 class Brand(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     formatter_name = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'brand'
 
 
 class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     code = models.SmallIntegerField()
     light = models.BooleanField()
     id_category = models.ForeignKey('self', models.DO_NOTHING, db_column='id_category')
 
     class Meta:
-        managed = False
         db_table = 'category'
 
 
 class Class(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     description = models.TextField()
 
     class Meta:
-        managed = False
         db_table = 'class'
 
 
@@ -80,7 +76,6 @@ class ClassCategory(models.Model):
     id_category = models.ForeignKey(Category, models.DO_NOTHING, db_column='id_category')
 
     class Meta:
-        managed = False
         db_table = 'class_category'
         unique_together = (('id_class', 'id_category'),)
 
@@ -90,7 +85,6 @@ class CoreBuilding(models.Model):
     stories_count = models.IntegerField()
 
     class Meta:
-        managed = False
         db_table = 'core_building'
 
 
@@ -101,11 +95,11 @@ class CoreStructure(models.Model):
     label = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
         db_table = 'core_structure'
 
 
 class Count(models.Model):
+    id = models.BigAutoField(primary_key=True)
     start_service_date = models.DateField()
     end_service_date = models.DateField()
     start_put_date = models.DateField(blank=True, null=True)
@@ -122,11 +116,11 @@ class Count(models.Model):
     id_installation = models.ForeignKey('Installation', models.DO_NOTHING, db_column='id_installation')
 
     class Meta:
-        managed = False
         db_table = 'count'
 
 
 class CountAggregate(models.Model):
+    id = models.BigAutoField(primary_key=True)
     type = models.TextField()
     start = models.DateTimeField()
     end = models.DateTimeField()
@@ -136,73 +130,73 @@ class CountAggregate(models.Model):
     id_lane = models.ForeignKey('Lane', models.DO_NOTHING, db_column='id_lane')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate'
 
 
 class CountAggregateValueCls(models.Model):
+    id = models.BigAutoField(primary_key=True)
     value = models.IntegerField()
     id_count_aggregate = models.ForeignKey(CountAggregate, models.DO_NOTHING, db_column='id_count_aggregate')
     id_category = models.ForeignKey(Category, models.DO_NOTHING, db_column='id_category')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate_value_cls'
 
 
 class CountAggregateValueCnt(models.Model):
+    id = models.BigAutoField(primary_key=True)
     value = models.SmallIntegerField()
     interval = models.SmallIntegerField()
     id_count_aggregate = models.ForeignKey(CountAggregate, models.DO_NOTHING, db_column='id_count_aggregate')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate_value_cnt'
 
 
 class CountAggregateValueDrn(models.Model):
+    id = models.BigAutoField(primary_key=True)
     value = models.SmallIntegerField()
     direction = models.SmallIntegerField()
     id_count_aggregate = models.ForeignKey(CountAggregate, models.DO_NOTHING, db_column='id_count_aggregate')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate_value_drn'
 
 
 class CountAggregateValueLen(models.Model):
+    id = models.BigAutoField(primary_key=True)
     value = models.SmallIntegerField()
     low = models.SmallIntegerField()
     high = models.SmallIntegerField()
     id_count_aggregate = models.ForeignKey(CountAggregate, models.DO_NOTHING, db_column='id_count_aggregate')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate_value_len'
 
 
 class CountAggregateValueSds(models.Model):
+    id = models.BigAutoField(primary_key=True)
     mean = models.FloatField()
     deviation = models.FloatField()
     id_count_aggregate = models.ForeignKey(CountAggregate, models.DO_NOTHING, db_column='id_count_aggregate')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate_value_sds'
 
 
 class CountAggregateValueSpd(models.Model):
+    id = models.BigAutoField(primary_key=True)
     value = models.SmallIntegerField()
     low = models.SmallIntegerField()
     high = models.SmallIntegerField()
     id_count_aggregate = models.ForeignKey(CountAggregate, models.DO_NOTHING, db_column='id_count_aggregate')
 
     class Meta:
-        managed = False
         db_table = 'count_aggregate_value_spd'
 
 
 class CountDetail(models.Model):
+    id = models.BigAutoField(primary_key=True)
     numbering = models.IntegerField()
     timestamp = models.DateTimeField()
     distance_front_front = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
@@ -219,43 +213,34 @@ class CountDetail(models.Model):
     id_category = models.ForeignKey(Category, models.DO_NOTHING, db_column='id_category')
 
     class Meta:
-        managed = False
         db_table = 'count_detail'
 
 
 class DamageLog(models.Model):
+    id = models.BigAutoField(primary_key=True)
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.TextField()
     id_device = models.ForeignKey('Device', models.DO_NOTHING, db_column='id_device')
 
     class Meta:
-        managed = False
         db_table = 'damage_log'
 
 
 class Device(models.Model):
+    id = models.BigAutoField(primary_key=True)
     serial = models.TextField(blank=True, null=True)
     purchase_date = models.DateField(blank=True, null=True)
     name = models.TextField()
     id_model = models.ForeignKey('Model', models.DO_NOTHING, db_column='id_model')
 
     class Meta:
-        managed = False
         db_table = 'device'
 
 
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
 
 class Installation(models.Model):
+    id = models.BigAutoField(primary_key=True)
     permanent = models.BooleanField()
     name = models.TextField()
     picture = models.TextField(blank=True, null=True)
@@ -263,11 +248,11 @@ class Installation(models.Model):
     active = models.BooleanField()
 
     class Meta:
-        managed = False
         db_table = 'installation'
 
 
 class Lane(models.Model):
+    id = models.BigAutoField(primary_key=True)
     number = models.SmallIntegerField()
     direction = models.SmallIntegerField()
     direction_desc = models.TextField(blank=True, null=True)
@@ -275,18 +260,17 @@ class Lane(models.Model):
     id_section = models.ForeignKey('Section', models.DO_NOTHING, db_column='id_section')
 
     class Meta:
-        managed = False
         db_table = 'lane'
 
 
 class Model(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     card_name = models.TextField(blank=True, null=True)
     configuration = models.TextField(blank=True, null=True)
     id_brand = models.ForeignKey(Brand, models.DO_NOTHING, db_column='id_brand')
 
     class Meta:
-        managed = False
         db_table = 'model'
 
 
@@ -295,7 +279,6 @@ class ModelClass(models.Model):
     id_class = models.ForeignKey(Class, models.DO_NOTHING, db_column='id_class')
 
     class Meta:
-        managed = False
         db_table = 'model_class'
         unique_together = (('id_model', 'id_class'),)
 
@@ -316,11 +299,11 @@ class Section(models.Model):
     end_validity = models.DateField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'section'
 
 
 class Sensor(models.Model):
+    id = models.BigAutoField(primary_key=True)
     id_lane = models.ForeignKey(Lane, models.DO_NOTHING, db_column='id_lane', blank=True, null=True)
     id_sensor_type = models.ForeignKey('SensorType', models.DO_NOTHING, db_column='id_sensor_type', blank=True, null=True)
     start_pr = models.TextField(blank=True, null=True)
@@ -332,16 +315,15 @@ class Sensor(models.Model):
     geometry = models.GeometryField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'sensor'
 
 
 class SensorType(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     permanent = models.BooleanField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'sensor_type'
 
 
@@ -350,7 +332,6 @@ class SensorTypeClass(models.Model):
     id_class = models.ForeignKey(Class, models.DO_NOTHING, db_column='id_class')
 
     class Meta:
-        managed = False
         db_table = 'sensor_type_class'
         unique_together = (('id_sensor_type', 'id_class'),)
 
@@ -360,7 +341,6 @@ class SensorTypeInstallation(models.Model):
     id_installation = models.ForeignKey(Installation, models.DO_NOTHING, db_column='id_installation')
 
     class Meta:
-        managed = False
         db_table = 'sensor_type_installation'
         unique_together = (('id_sensor_type', 'id_installation'),)
 
@@ -370,22 +350,22 @@ class SensorTypeModel(models.Model):
     id_model = models.ForeignKey(Model, models.DO_NOTHING, db_column='id_model')
 
     class Meta:
-        managed = False
         db_table = 'sensor_type_model'
         unique_together = (('id_sensor_type', 'id_model'),)
 
 
 class SensorTypeSection(models.Model):
+    id = models.BigAutoField(primary_key=True)
     geometry = models.GeometryField(blank=True, null=True)
     id_sensor_type = models.ForeignKey(SensorType, models.DO_NOTHING, db_column='id_sensor_type', blank=True, null=True)
     id_section = models.ForeignKey(Section, models.DO_NOTHING, db_column='id_section', blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'sensor_type_section'
 
 
 class SpecialPeriod(models.Model):
+    id = models.BigAutoField(primary_key=True)
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.TextField()
@@ -393,5 +373,4 @@ class SpecialPeriod(models.Model):
     influence = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'special_period'
