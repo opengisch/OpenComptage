37fc2cd896c12b6dd6de36ae5e18a9fafc954e05
olivierdalang <olivier.dalang@gmail.com>
2021-06-09 15:44:07 +0200

    (full config)

--- comptages/datamodel/config.py
+++ comptages/datamodel/config.py
@@ -1,6 +1,8 @@
 DATAMODEL_NAME = "comptages"
 DATABASE = {
     "ENGINE": "django.contrib.gis.db.backends.postgis",
+    "HOST": "127.0.0.1",
+    "PORT": "5432",
     "NAME": "comptages",
     "USER": "postgres",
     "PASSWORD": "postgres",
