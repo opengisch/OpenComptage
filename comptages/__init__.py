48c338cea7e6881bb332d14a2a8a34d94228866b
Mario Baranzini <mario@opengis.ch>
2021-07-07 15:11:35 +0200

    Register datamodel earlier

--- comptages/__init__.py
+++ comptages/__init__.py
@@ -1,3 +1,14 @@
+from comptages.datamodel import config
+
+# Register the datamodel
+try:
+    from qdmtk import register_datamodel
+    register_datamodel(config.DATAMODEL_NAME, config.INSTALLED_APPS, config.DATABASE)
+except ImportError:
+    pass
+    #    self.iface.messageBar().pushMessage("Could not load QDMTK.", "You must install the QDMTK plugin prior to using Comptages", level=Qgis.Critical)
+
+
 def classFactory(iface):
     """Load Comptages class from file comptages.
 
--- comptages/comptages.py
+++ comptages/comptages.py
@@ -27,7 +27,6 @@ from comptages.report.yearly_report_creator import YearlyReportCreator
 from comptages.ics.ics_importer import IcsImporter
 from comptages.ui.resources import *
 
-from .datamodel import config
 
 class Comptages(QObject):
 
@@ -45,14 +44,6 @@ class Comptages(QObject):
         self.filter_installation = None
         self.filter_sensor = None
         self.tm = QgsApplication.taskManager()
-
-        # Register the datamodel
-        try:
-            from qdmtk import register_datamodel
-            register_datamodel(config.DATAMODEL_NAME, config.INSTALLED_APPS, config.DATABASE)
-        except ImportError:
-            self.iface.messageBar().pushMessage("Could not load QDMTK.", "You must install the QDMTK plugin prior to using Comptages", level=Qgis.Critical)
-
 
     def initGui(self):
         self.connect_db_action = QAction(
