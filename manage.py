81c799033168272345103fb74bc973652d8b7aea
Mario Baranzini <mario@opengis.ch>
2021-07-08 08:47:43 +0200

    Fix duplicated register_datamodel

--- manage.py
+++ manage.py
@@ -1,10 +1,6 @@
-import os
 import sys
 from django.core.management import execute_from_command_line
-from comptages.datamodel import config
-from qdmtk import register_datamodel
 
 
 if __name__ == "__main__":
-    register_datamodel(config.DATAMODEL_NAME, config.INSTALLED_APPS, config.DATABASE)
     execute_from_command_line(sys.argv)
