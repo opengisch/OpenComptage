024f02b966e312ef9026bda30e775d8714ec385d
Mario Baranzini <mario@opengis.ch>
2021-07-08 08:53:47 +0200

    Fix register_datamodel

--- manage.py
+++ manage.py
@@ -1,6 +1,13 @@
 import sys
 from django.core.management import execute_from_command_line
+from comptages.datamodel import config
+from qdmtk import register_datamodel
 
 
 if __name__ == "__main__":
+    try:
+        register_datamodel(config.DATAMODEL_NAME, config.INSTALLED_APPS, config.DATABASE)
+    except Exception:
+        pass
+
     execute_from_command_line(sys.argv)
