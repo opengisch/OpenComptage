22daff4a3b04dbd6c5d2b8d48cd28c55d8c6df32
Mario Baranzini <mario@opengis.ch>
2021-11-10 10:44:03 +0100

    Importer

--- opencomptage/core/importer.py
+++ opencomptage/core/importer.py
@@ -1,6 +1,6 @@
 import pytz
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from opencomptage.datamodel import models
 from opencomptage.core.bulk_create_manager import BulkCreateManager
@@ -9,57 +9,78 @@ IMPORT_STATUS_QUARANTINE = 1
 IMPORT_STATUS_DEFINITIVE = 0
 
 
-def import_file(file_path, count):
-
-    # TODO: check format
-    # TODO: all formats
-
-    _parse_and_write(file_path, count)
-
-
 def simple_print_callback(progress):
     print(f"Importing... {progress}%")
 
 
-def _parse_and_write(file_path, count, callback_progress=simple_print_callback):
+def import_file(file_path, count, callback_progress=simple_print_callback):
+
+    file_format = get_file_format(file_path)
+    file_header = _parse_file_header(file_path)
+
+    if file_format == "VBV-1":
+        _parse_and_write(file_path, count, _parse_line_vbv1, callback_progress)
+    elif file_format == "INT-2":
+        interval = int(file_header['INTERVAL'])
+        intspec = get_intspec(file_header)
+        data_header = _parse_data_header(file_path)
+        cat_bins = _populate_category_dict(count)
+        _parse_and_write(
+            file_path, count,
+            _parse_line_int2,
+            callback_progress,
+            interval=interval,
+            intspec=intspec,
+            data_header=data_header,
+            file_header=file_header,
+            categories=cat_bins,
+        )
+    elif file_format == "MC":
+        _parse_and_write(file_path, count, _parse_line_mc, callback_progress)
+    else:
+        raise NotImplementedError("file format not recognized")
+
+
+def _parse_and_write(file_path, count, line_parser, callback_progress, **kwargs):
 
     basename = os.path.basename(file_path)
     bulk_mgr = BulkCreateManager(chunk_size=1000)
     lanes = _populate_lane_dict(count)
     cat_bins = _populate_category_dict(count)
+    line_count = get_line_count(file_path)
     previous_progress = 0
     try:
-        with open(file_path) as f:
-            number_of_lines = sum(1 for _ in f)
-            with open(file_path) as f:
-                for i, line in enumerate(f):
-                    progress = int(100 / number_of_lines * i)
+        with open(file_path, encoding=get_file_encoding(file_path)) as f:
+            for i, line in enumerate(f):
+                rows = line_parser(line, **kwargs)
+                if not rows:
+                    continue
 
-                    if not progress == previous_progress:
-                        callback_progress(progress)
-                        previous_progress = progress
+                progress = int(100 / line_count * i)
 
-                    if not line.startswith('* '):
-                        row = _parse_line(line)
-                        if not row:
-                            return
+                if not progress == previous_progress:
+                    callback_progress(progress)
+                    previous_progress = progress
 
-                        bulk_mgr.add(
-                            models.CountDetail(
-                                numbering=row['numbering'],
-                                timestamp=row['timestamp'],
-                                distance_front_front=row['distance_front_front'],
-                                distance_front_back=row['distance_front_back'],
-                                speed=row['speed'],
-                                length=row['length'],
-                                height=row['height'],
-                                file_name=basename,
-                                import_status=IMPORT_STATUS_QUARANTINE,
-                                id_lane_id=lanes[int(row['lane'])],
-                                id_count_id=count.id,
-                                id_category_id=cat_bins[row['category']]
-                            )
+                for row in rows:
+                    category = cat_bins[row['category']] if row['category'] else None
+                    bulk_mgr.add(
+                        models.CountDetail(
+                            numbering=row['numbering'],
+                            timestamp=row['timestamp'],
+                            distance_front_front=row['distance_front_front'],
+                            distance_front_back=row['distance_front_back'],
+                            speed=row['speed'],
+                            length=row['length'],
+                            height=row['height'],
+                            file_name=basename,
+                            import_status=IMPORT_STATUS_QUARANTINE,
+                            id_lane_id=lanes[int(row['lane'])],
+                            id_count_id=count.id,
+                            id_category_id=category,
+                            times=row['times'],
                         )
+                    )
 
     except Exception as e:
         raise e
@@ -67,7 +88,11 @@ def _parse_and_write(file_path, count, callback_progress=simple_print_callback):
     bulk_mgr.done()
 
 
-def _parse_line(line):
+def _parse_line_vbv1(line, **kwargs):
+
+    if line.startswith('* '):
+        return None
+
     parsed_line = None
     try:
         tz = pytz.timezone('Europe/Zurich')
@@ -85,6 +110,7 @@ def _parse_line(line):
         parsed_line['length'] = int(line[52:56])
         parsed_line['category'] = int(line[60:62].strip())
         parsed_line['height'] = line[63:65].strip()
+        parsed_line['times'] = 1
 
         # If the speed of a vehicle is 0, we put it in the category 0
         if parsed_line['speed'] == 0:
@@ -115,7 +141,196 @@ def _parse_line(line):
         if 'height ' not in parsed_line:
             parsed_line['height'] = 'NA'
 
-    return parsed_line
+    return [parsed_line]
+
+
+def _parse_line_mc(line, **kwargs):
+    if not line.startswith('20'):
+        return None
+
+    parsed_line = None
+    try:
+        parsed_line = dict()
+        tz = pytz.timezone('Europe/Zurich')
+        # TODO: numbering
+        numbering = 1
+        parsed_line['numbering'] = numbering
+        parsed_line['timestamp'] = tz.localize(datetime.strptime(
+            line[0:19], "%Y-%m-%d %H:%M:%S"))
+        # On MetroCount files, the direction is 0-1 instead of 1-2
+        parsed_line['lane'] = int(line[22:23]) + 1
+        parsed_line['direction'] = int(line[22:23]) + 1
+        parsed_line['distance_front_front'] = float(line[24:31])
+        if parsed_line['distance_front_front'] > 99.9:
+            parsed_line['distance_front_front'] = 99.9
+        parsed_line['distance_front_back'] = float(line[31:38])
+        if parsed_line['distance_front_back'] > 99.9:
+            parsed_line['distance_front_back'] = 99.9
+        parsed_line['speed'] = int(float(line[39:44]))
+        parsed_line['length'] = int(float(line[44:50]))
+        parsed_line['category'] = int(line[51:54].strip())
+        parsed_line['height'] = ''
+        parsed_line['times'] = 1
+    except ValueError as e:
+        # QgsMessageLog.logMessage(
+        #     'ValueError: {}'.format(e),  'Comptages', Qgis.Info)
+
+        # This can happen when some values are missed from a line
+        return None
+
+    return [parsed_line]
+
+def _parse_line_int2(line, **kwargs):
+    if line.startswith('* '):
+        return None
+
+    parsed_line = dict()
+    tz = pytz.timezone('Europe/Zurich')
+    # TODO: numbering
+    numbering = 1
+    parsed_line['numbering'] = numbering
+    # In the data files midnight is 2400 of the current day
+    # instead of 0000 of the next day
+    if line[7:9] == '24':
+        line = line[:7] + '00' + line[9:]
+        end = tz.localize(datetime.strptime(
+            "{}".format(line[0:11]), "%d%m%y %H%M"))
+        end += timedelta(days=1)
+    else:
+        end = tz.localize(datetime.strptime(
+            "{}".format(line[0:11]), "%d%m%y %H%M"))
+
+    parsed_line['end'] = end
+    parsed_line['start'] = parsed_line['end'] - timedelta(
+        minutes=kwargs['interval'])
+    parsed_line['channel'] = line[12:13]
+    parsed_line['reserve_code'] = line[14:16]
+    parsed_line['info_code'] = line[17:19]
+
+    parsed_line['timestamp'] = parsed_line['start']
+
+    parsed_line['distance_front_front'] = None
+    parsed_line['distance_front_back'] = None
+    parsed_line['speed'] = None
+    parsed_line['length'] = None
+    parsed_line['height'] = None
+    parsed_line['category'] = None
+    parsed_line['lane'] = 1
+    parsed_line['times'] = 1
+
+    intspec = kwargs['intspec']
+    row_type = intspec[int(parsed_line['info_code']) - 1]
+    bins = _get_int_bins(
+        kwargs['file_header'],
+        kwargs['data_header'],
+        kwargs['intspec'],
+        kwargs['categories'],
+        row_type)
+    if row_type == 'SPD':
+        for i, data in enumerate(line[20:].split()):
+            speed_low = bins[i]
+            speed = int(int(speed_low) + 5)
+            parsed_line['speed'] = speed
+            parsed_line['times'] = data
+            yield parsed_line
+    elif row_type == 'LEN':
+        for i, data in enumerate(line[20:].split()):
+            lenght_low = bins[i]
+            lenght_high = bins[i + 1]
+            lenght = int(int(lenght_low) + int(lenght_high) / 2)
+            parsed_line['lenght'] = lenght
+            parsed_line['times'] = data
+            yield parsed_line
+    elif row_type == 'CLS':
+        for i, data in enumerate(line[20:].split()):
+            parsed_line['category'] = i + 1
+            parsed_line['times'] = data
+            yield parsed_line
+    elif row_type == 'SDS':
+        # Insert the values in the SPD table and only the
+        # mean and the deviation in the SDS table
+        pass
+    elif row_type == 'DRN':
+        pass
+    elif row_type == 'CNT':
+        pass
+
+    return None
+
+
+def _get_int_bins(file_header, data_header, intspec, categories, code):
+    """Returns an array with the bins if they exist, or the number of
+    columns of this data type"""
+    values = []
+    if code == 'SPD' or code == 'SDS':
+        values = file_header['SPDBINS'].split()
+    elif code == 'LEN':
+        values = file_header['LENBINS'].split()
+    elif code == 'CLS':
+        values = list(categories.values())[1:]  # [1:] is because 0 is trash
+    else:
+        values = data_header[intspec.index(code)]
+    return values
+
+
+def _parse_file_header(file_path):
+    file_header = dict()
+
+    with open(file_path, encoding=get_file_encoding(file_path)) as f:
+        for line in f:
+            # Marksmann
+            if line.startswith('* ') and not line.startswith('* HEAD '):
+                line = line[2:]
+                splitted = line.split('=', 1)
+                if len(splitted) > 1:
+                    key = splitted[0].strip()
+                    value = splitted[1].strip()
+                    if key == 'CLASS' and value == 'SPECIAL10':
+                        value = 'SWISS10'
+                    file_header[key] = value
+            # MetroCount
+            elif line.startswith('MetroCount'):
+                file_header['FORMAT'] = 'MC'
+            elif line.startswith('Place'):
+                file_header['SITE'] = line[
+                    line.find('[') + 1:line.find(']')].replace('-', '')
+            elif line.startswith('20') and file_header['FORMAT'] == 'MC' and 'STARTREC' not in file_header:
+                file_header['STARTREC'] = datetime.strftime(
+                    datetime.strptime(line[:19], "%Y-%m-%d %H:%M:%S"),
+                    "%H:%M %d/%m/%y")
+            elif line.startswith('20') and file_header['FORMAT'] == 'MC':
+                file_header['STOPREC'] = datetime.strftime(
+                    datetime.strptime(line[:19], "%Y-%m-%d %H:%M:%S"),
+                    "%H:%M %d/%m/%y")
+            elif line.startswith('Type de Cat') and file_header['FORMAT'] == 'MC':
+                file_header['CLASS'] = line[line.find('(') + 1:line.find(')')]
+                if file_header['CLASS'] == 'Euro13':
+                    file_header['CLASS'] = 'EUR13'
+                elif file_header['CLASS'] == 'NZTA2011':
+                    file_header['CLASS'] = 'NZ13'
+                elif file_header['CLASS'][:5] == 'FHWA ':
+                    file_header['CLASS'] = 'FHWA13'
+                elif file_header['CLASS'] == 'CAT-Cycle_dist-empat':
+                    file_header['CLASS'] = 'SPCH-MD 5C'
+    return file_header
+
+
+def _parse_data_header(file_path):
+    data_header = []
+
+    with open(file_path, encoding=get_file_encoding(file_path)) as f:
+        for line in f:
+            if line.startswith('* HEAD '):
+                start_char = 20
+                i = 0
+                while True:
+                    if not line[start_char:start_char + 4] == '':
+                        i += 1
+                        start_char += 5
+                    else:
+                        data_header.append(i)
+                        break
+    return data_header
 
 
 def _populate_lane_dict(count):
@@ -147,3 +362,44 @@ def _populate_category_dict(count):
     ).order_by('code')
 
     return {x.code: x.id for x in categories}
+
+
+def get_file_format(file_path):
+    """Return the file format on None if not identified."""
+    with open(file_path, encoding=get_file_encoding(file_path)) as f:
+        for line in f:
+            line = f.readline()
+            if line.startswith("* FORMAT"):
+                return line.split("=", 1)[1].strip()
+            elif line.startswith("MetroCount"):
+                return "MC"
+
+        return None
+
+
+def get_file_encoding(file_path):
+    """Guess the right file encoding."""
+    with open(file_path, 'r', encoding='utf-8') as f:
+        try:
+            for line in f:
+                pass
+        except UnicodeDecodeError:
+            return 'ISO-8859-1'
+        else:
+            return 'utf-8'
+
+
+def get_line_count(file_path):
+    with open(file_path, encoding=get_file_encoding(file_path)) as f:
+        number_of_lines = sum(1 for _ in f)
+    return number_of_lines
+
+
+def get_intspec(file_header):
+    intspec = []
+    for i, code in enumerate(file_header['INTSPEC'].split('+')):
+        if code.strip() not in ['SPD', 'SDS', 'LEN', 'CLS', 'CNT', 'DRN']:
+            raise NotImplementedError('{}'.format(code.strip()))
+        # the key corrpespond to the value in the data row
+        intspec.append(code.strip())
+    return intspec
--- opencomptage/test/test_opencomptage.py
+++ opencomptage/test/test_opencomptage.py
@@ -13,15 +13,14 @@ class OpenComptageTest(TransactionTestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Import basedata
-        call_command("importdata")
-
-    def setUp(self):
-        # TODO: delete all count, countDetail and tjm
         pass
 
-    def test_import_vbv1(self):
+    def setUp(self):
+        # With TransactionTestCase the db is reset at every test, so we
+        # re-import base data every time.
+        call_command("importdata")
 
+    def test_import_vbv1(self):
         model = models.Model.objects.all()[0]
         device = models.Device.objects.all()[0]
         sensor_type = models.SensorType.objects.all()[0]
@@ -42,7 +41,7 @@ class OpenComptageTest(TransactionTestCase):
             id_installation=installation,
         )
 
-        importer._parse_and_write(
+        importer.import_file(
             utils.test_data_path("00056520.V01"),
             count)
 
@@ -56,9 +55,7 @@ class OpenComptageTest(TransactionTestCase):
         self.assertEqual(first, tz.localize(datetime(2021, 10, 15, 9, 46, 43, 500000)))
         self.assertEqual(last, tz.localize(datetime(2021, 10, 15, 23, 59, 54, 600000)))
 
-
     def test_import_mc(self):
-
         model = models.Model.objects.all()[0]
         device = models.Device.objects.all()[0]
         sensor_type = models.SensorType.objects.all()[0]
@@ -79,8 +76,90 @@ class OpenComptageTest(TransactionTestCase):
             id_installation=installation,
         )
 
-        importer._parse_and_write(
+        importer.import_file(
             utils.test_data_path("64210836_TCHO-Capitaine.txt"),
             count)
 
         self.assertEqual(models.CountDetail.objects.count(), 25867)
+
+        tz = pytz.timezone("Europe/Zurich")
+
+        first = tz.normalize(models.CountDetail.objects.first().timestamp)
+        last = tz.normalize(models.CountDetail.objects.last().timestamp)
+
+        self.assertEqual(first, tz.localize(datetime(2021, 9, 10, 4, 16, 16)))
+        self.assertEqual(last, tz.localize(datetime(2021, 9, 21, 8, 2, 15)))
+
+    def test_import_int2(self):
+        model = models.Model.objects.all()[0]
+        device = models.Device.objects.all()[0]
+        sensor_type = models.SensorType.objects.all()[0]
+        class_ = models.Class.objects.get(name="SWISS10")
+        installation = models.Installation.objects.get(name="10020260")
+
+        count = models.Count.objects.create(
+            start_service_date=datetime(2018, 4, 13),
+            end_service_date=datetime(2018, 5, 1),
+            start_process_date=datetime(2018, 4, 13),
+            end_process_date=datetime(2018, 5, 1),
+            start_put_date=datetime(2018, 4, 13),
+            end_put_date=datetime(2018, 5, 1),
+            id_model=model,
+            id_device=device,
+            id_sensor_type=sensor_type,
+            id_class=class_,
+            id_installation=installation,
+        )
+
+        importer.import_file(
+            utils.test_data_path("10020260.A01"),
+            count)
+
+        self.assertEqual(models.CountDetail.objects.count(), 22568)
+
+        tz = pytz.timezone("Europe/Zurich")
+
+        first = tz.normalize(models.CountDetail.objects.first().timestamp)
+        last = tz.normalize(models.CountDetail.objects.last().timestamp)
+
+        self.assertEqual(first, tz.localize(datetime(2018, 4, 13, 12, 0)))
+        self.assertEqual(last, tz.localize(datetime(2018, 5, 1, 13, 0)))
+
+    def test_import_simple_int2(self):
+        model = models.Model.objects.all()[0]
+        device = models.Device.objects.all()[0]
+        sensor_type = models.SensorType.objects.all()[0]
+        class_ = models.Class.objects.get(name="SWISS10")
+        installation = models.Installation.objects.get(name="64080011")
+
+        count = models.Count.objects.create(
+            start_service_date=datetime(2018, 9, 24),
+            end_service_date=datetime(2018, 9, 24),
+            start_process_date=datetime(2018, 9, 24),
+            end_process_date=datetime(2018, 9, 24),
+            start_put_date=datetime(2018, 9, 24),
+            end_put_date=datetime(2018, 9, 24),
+            id_model=model,
+            id_device=device,
+            id_sensor_type=sensor_type,
+            id_class=class_,
+            id_installation=installation,
+        )
+
+        importer.import_file(
+            utils.test_data_path("simple_aggregate_multi_spec.i00"),
+            count)
+
+        self.assertEqual(models.CountDetail.objects.count(), 52)
+
+        tz = pytz.timezone("Europe/Zurich")
+
+        first = tz.normalize(models.CountDetail.objects.first().timestamp)
+        last = tz.normalize(models.CountDetail.objects.last().timestamp)
+
+        self.assertEqual(first, tz.localize(datetime(2018, 9, 24, 0, 0)))
+        self.assertEqual(last, tz.localize(datetime(2018, 9, 24, 1, 0)))
+
+        speed20 = models.CountDetail.objects.filter(speed=20)
+        self.assertEqual(speed20[0].times, 3)
+        self.assertEqual(speed20[1].times, 4)
