934cfc9cf2732e44ff8591ee2fb3af73590442e5
spch-GL <pierre.grellet@ne.ch>
2021-12-02 05:46:58 +0100

    uniformReport_infos_layers (#224)

--- comptages/core/definitions.py
+++ comptages/core/definitions.py
@@ -4,7 +4,7 @@ IMPORT_STATUS_DEFINITIVE = 0
 
 # Definitions of the layers
 LAYER_DEFINITIONS = {
-    'installation':        {
+    'installation': {
         'table':        'installation',
         'display_name': 'installation',
         'geometry':     'geometry',
@@ -13,7 +13,16 @@ LAYER_DEFINITIONS = {
         'epsg':         'EPSG:2056',
         'legend':       True
     },
-    'section':        {
+    'sensor': {
+        'table':        'sensor',
+        'display_name': 'capteur',
+        'geometry':     'geometry',
+        'sql':          '',
+        'id':           'id',
+        'epsg':         'EPSG:2056',
+        'legend':       True
+    },
+    'section': {
         'table':        'section',
         'display_name': 'troncon',
         'geometry':     'geometry',
@@ -22,23 +31,23 @@ LAYER_DEFINITIONS = {
         'epsg':         'EPSG:2056',
         'legend':       True
     },
-    'lane':        {
-        'table':        'lane',
-        'display_name': 'voie',
+    'sector': {
+        'table':        'sector',
+        'display_name': 'secteur',
+        'geometry':     'geometry',
+        'sql':          '',
+        'id':           'id',
+        'epsg':         'EPSG:2056',
+        'legend':       True
+    },
+    'brand': {
+        'table':        'brand',
+        'display_name': 'marque',
         'geometry':     None,
         'sql':          '',
         'id':           'id',
         'epsg':         'EPSG:2056',
         'legend':       False
-    },
-    'count': {
-        'table':        'count',
-        'display_name': 'comptage',
-        'geometry':     None,
-        'sql':          '',
-        'id':           'id',
-        'epsg':         'EPSG:2056',
-        'legend':       True
     },
     'model': {
         'table':        'model',
@@ -58,45 +67,9 @@ LAYER_DEFINITIONS = {
         'epsg':         'EPSG:2056',
         'legend':       False
     },
-    'sensor_type': {
-        'table':        'sensor_type',
-        'display_name': 'type_capteur',
-        'geometry':     None,
-        'sql':          '',
-        'id':           'id',
-        'epsg':         'EPSG:2056',
-        'legend':       False
-    },
     'class': {
         'table':        'class',
         'display_name': 'classification',
-        'geometry':     None,
-        'sql':          '',
-        'id':           'id',
-        'epsg':         'EPSG:2056',
-        'legend':       False
-    },
- #   'count_detail': {
- #       'table':        'count_detail',
- #       'display_name': 'comptage_detail',
- #       'geometry':     None,
- #       'sql':          '',
- #       'id':           'id',
- #       'epsg':         'EPSG:2056',
- #       'legend':       False
- #   },
-    'damage_log': {
-        'table':        'damage_log',
-        'display_name': 'journal_panne',
-        'geometry':     None,
-        'sql':          '',
-        'id':           'id',
-        'epsg':         'EPSG:2056',
-        'legend':       False
-    },
-    'brand': {
-        'table':        'brand',
-        'display_name': 'marque',
         'geometry':     None,
         'sql':          '',
         'id':           'id',
@@ -112,6 +85,33 @@ LAYER_DEFINITIONS = {
         'epsg':         'EPSG:2056',
         'legend':       False
     },
+#     'count_detail': {
+#         'table':        'count_detail',
+#         'display_name': 'comptage_detail',
+#         'geometry':     None,
+#         'sql':          '',
+#         'id':           'id',
+#         'epsg':         'EPSG:2056',
+#         'legend':       False
+#     },
+#     'count_aggregate': {
+#         'table':        'count_aggregate',
+#         'display_name': 'comptage_aggrege',
+#         'geometry':     None,
+#         'sql':          '',
+#         'id':           'id',
+#         'epsg':         'EPSG:2056',
+#         'legend':       False
+#     },
+    'damage_log': {
+        'table':        'damage_log',
+        'display_name': 'journal_panne',
+        'geometry':     None,
+        'sql':          '',
+        'id':           'id',
+        'epsg':         'EPSG:2056',
+        'legend':       False
+    },
     'special_period': {
         'table':        'special_period',
         'display_name': 'periode_speciale',
@@ -121,13 +121,31 @@ LAYER_DEFINITIONS = {
         'epsg':         'EPSG:2056',
         'legend':       False
     },
-    'sector': {
-        'table':        'sector',
-        'display_name': 'secteur',
-        'geometry':     'geometry',
+    'count': {
+        'table':        'count',
+        'display_name': 'comptage',
+        'geometry':     None,
         'sql':          '',
         'id':           'id',
         'epsg':         'EPSG:2056',
         'legend':       True
+    },
+    'sensor_type': {
+        'table':        'sensor_type',
+        'display_name': 'type_capteur',
+        'geometry':     None,
+        'sql':          '',
+        'id':           'id',
+        'epsg':         'EPSG:2056',
+        'legend':       False
+    },
+    'lane': {
+        'table':        'lane',
+        'display_name': 'voie',
+        'geometry':     None,
+        'sql':          '',
+        'id':           'id',
+        'epsg':         'EPSG:2056',
+        'legend':       False
     },
 }
