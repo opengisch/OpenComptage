53ef6e4c6e66eef3f8af71961d7249ac44b45c7c
why-not-try-calmer <mrnycticorax@gmail.com>
2023-11-27 14:15:23 +0100

    ensure all items are there

--- comptages/test/test_report.py
+++ comptages/test/test_report.py
@@ -45,3 +45,33 @@ class ImportTest(TransactionTestCase):
         importer.import_file(utils.test_data_path("00056520.V02"), count)
 
         report.prepare_reports("/tmp/", count)
+
+    def test_no_missing_items_from_count_details(self):
+        file_name = "64540060_Latenium_PS2021_ChMixte.txt"
+        installation_name = "64540060"
+
+        model = models.Model.objects.all()[0]
+        device = models.Device.objects.all()[0]
+        sensor_type = models.SensorType.objects.all()[0]
+        class_ = models.Class.objects.get(name="SPCH13")
+        installation = models.Installation.objects.get(name=installation_name)
+
+        count = models.Count.objects.create(
+            start_service_date=datetime(2021, 1, 5),
+            end_service_date=datetime(2021, 12, 1),
+            start_process_date=datetime(2021, 1, 10),
+            end_process_date=datetime(2021, 11, 1),
+            start_put_date=datetime(2021, 1, 1),
+            end_put_date=datetime(2021, 12, 31),
+            id_model=model,
+            id_device=device,
+            id_sensor_type=sensor_type,
+            id_class=class_,
+            id_installation=installation,
+        )
+
+        importer.import_file(utils.test_data_path(file_name), count)
+        items = models.CountDetail.objects.filter(
+            id_count=count.id, timestamp__gt="2021-03-02", timestamp__lt="2021-03-03"
+        )
+        self.assertEqual(items.count(), 360)
