7720a4695bcd31d473a1d0289cbcbba4cf809b78
why-not-try-calmer <mrnycticorax@gmail.com>
2023-11-27 13:52:51 +0100

    loading some test data and creating some outputs

--- comptages/test/test_report.py
+++ comptages/test/test_report.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytz
 from datetime import datetime
 from django.test import TransactionTestCase
@@ -45,3 +46,35 @@ class ImportTest(TransactionTestCase):
         importer.import_file(utils.test_data_path("00056520.V02"), count)
 
         report.prepare_reports("/tmp/", count)
+
+    def test_report_ensure_first_sections(self):
+        # Create count and import some data pertaining to
+        # a specific special case
+        section_id = "53526896"
+        test_data_folder = "5350_1_4"
+        
+        model = models.Model.objects.all()[0]
+        device = models.Device.objects.all()[0]
+        sensor_type = models.SensorType.objects.all()[0]
+        class_ = models.Class.objects.get(name="SWISS10")
+        installation = models.Installation.objects.get(lane__id_section_id=section_id)
+        tz = pytz.timezone("Europe/Zurich")
+
+        count = models.Count.objects.create(
+            start_service_date=tz.localize(datetime(2021, 3, 1)),
+            end_service_date=tz.localize(datetime(2021, 3, 2)),
+            start_process_date=tz.localize(datetime(2021, 3, 15)),
+            end_process_date=tz.localize(datetime(2021, 3, 28)),
+            start_put_date=tz.localize(datetime(2021, 2, 20)),
+            end_put_date=tz.localize(datetime(2021, 4, 1)),
+            id_model=model,
+            id_device=device,
+            id_sensor_type=sensor_type,
+            id_class=class_,
+            id_installation=installation,
+        )
+
+        for file in Path(utils.test_data_path(test_data_folder)).iterdir():
+            importer.import_file(utils.test_data_path(str(file)), count)
+        
+        report.prepare_reports("/OpenComptage/testoutputs", count)
