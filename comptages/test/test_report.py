6bb7f47df19433db3b5421a11a4c0b92684a28f2
why-not-try-calmer <mrnycticorax@gmail.com>
2023-12-01 10:52:53 +0100

    test

--- comptages/test/test_report.py
+++ comptages/test/test_report.py
@@ -1,13 +1,15 @@
 import decimal
 from itertools import chain
+import os
+from pathlib import Path
 from django.test import TransactionTestCase
 from django.core.management import call_command
 from django.db.models.manager import Manager
-from comptages.report.yearly_report_bike import YearlyReportBike
 
-from comptages.test import utils, yearly_count_for
-from comptages.datamodel import models
 from comptages.core import report, importer
+from comptages.datamodel import models
+from comptages.report.yearly_report_bike import YearlyReportBike
+from comptages.test import utils, yearly_count_for
 
 
 class ImportTest(TransactionTestCase):
@@ -32,61 +34,18 @@ class ImportTest(TransactionTestCase):
         importer.import_file(utils.test_data_path("00056520.V02"), count)
         report.prepare_reports("/tmp/", count)
 
-    def test_no_missing_items_from_count_details(self):
+    def test_ensure_non_rounded_values(self):
         file_name = "64540060_Latenium_PS2021_ChMixte.txt"
         installation_name = "64540060"
-        model = models.Model.objects.all()[0]
-        device = models.Device.objects.all()[0]
-        sensor_type = models.SensorType.objects.all()[0]
-        class_ = models.Class.objects.get(name="SPCH13")
         installation = models.Installation.objects.get(name=installation_name)
-        sections_ids = models.Section.objects.filter(
-            lane__id_installation_id=installation.id
-        ).values_list("id", flat=True)
-        self.assertEqual(len(sections_ids), 2)
-
-        tz = pytz.timezone("Europe/Zurich")
-        count = models.Count.objects.create(
-            start_service_date=tz.localize(datetime(2021, 1, 5)),
-            end_service_date=tz.localize(datetime(2021, 12, 1)),
-            start_process_date=tz.localize(datetime(2021, 1, 10)),
-            end_process_date=tz.localize(datetime(2021, 11, 1)),
-            start_put_date=tz.localize(datetime(2021, 1, 1)),
-            end_put_date=tz.localize(datetime(2021, 12, 31)),
-        installation = models.Installation.objects.get(name="64210836")
-
-        count = models.Count.objects.create(
-            start_service_date=datetime(2021, 9, 10),
-            end_service_date=datetime(2021, 9, 21),
-            start_process_date=datetime(2021, 9, 10),
-            end_process_date=datetime(2021, 9, 21),
-            start_put_date=datetime(2021, 9, 10),
-            end_put_date=datetime(2021, 9, 21),
-            id_model=model,
-            id_device=device,
-            id_sensor_type=sensor_type,
-            id_class=class_,
-            id_installation=installation,
-        )
-
-        importer.import_file(utils.test_data_path(file_name), count)
-        items = models.CountDetail.objects.filter(
-            id_count=count.id, timestamp__gt="2021-03-02", timestamp__lt="2021-03-03"
-        )
-        self.assertEqual(items.count(), 360)
-
-    def test_ensure_non_rounded_values(self):
-    def test_ensure_non_rounded_values(self):
-        installation = models.Installation.objects.get(name="00056520")
         count = yearly_count_for(2021, installation)
-        importer.import_file(utils.test_data_path("64210836_TCHO-Capitaine.txt"), count)
-        lanes_installation: Manager = installation.lane_set
-        self.assertIsNotNone(lanes_installation)
+        importer.import_file(utils.test_data_path(file_name), count)
 
-        section_id = models.Section.objects.get(
-            lane__id=lanes_installation.values_list("pk", flat=True)[0]
-        )
-        report = YearlyReportBike("template_yearly_bike.xlsx", 2021, section_id)
+        lanes_installation: Manager = installation.lane_set
+        section_ids = lanes_installation.values_list("id_section", flat=True)
+        self.assertTrue(section_ids.exists())
+
+        report = YearlyReportBike("template_yearly_bike.xlsx", 2021, section_ids[0])
         report_dir1 = report.values_by_hour_and_direction(1).values_list(
             "tjm", flat=True
         )
